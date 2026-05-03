49317209e39f77e74658ff147f49739d43bb02b5
lucas3012 <lukinhas87.let@gmail.com>
2026-05-03 20:57:05 -0300

    Sistema atualizado - Link: https://produce-weights-choir-cdt.trycloudflare.com

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // O ficheiro iniciar.sh irá preencher esta variável automaticamente entre as aspas
-const BASE_URL = 'https://diagnostic-milk-qualified-downloaded.trycloudflare.com';
+const BASE_URL = 'https://produce-weights-choir-cdt.trycloudflare.com';
 
 /**
  * Função Inteligente de URL
