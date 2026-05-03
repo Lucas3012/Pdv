b8a6df3d266448db30b3ea81f2f42f9d8dc3a75e
lucas3012 <lukinhas87.let@gmail.com>
2026-05-03 19:18:31 -0300

    Sistema atualizado - Link: https://summer-diy-copyrights-save.trycloudflare.com

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // O ficheiro iniciar.sh irá preencher esta variável automaticamente entre as aspas
-const BASE_URL = 'https://tumor-personals-season-pic.trycloudflare.com';
+const BASE_URL = 'https://summer-diy-copyrights-save.trycloudflare.com';
 
 /**
  * Função Inteligente de URL
